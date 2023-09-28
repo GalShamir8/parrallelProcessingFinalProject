cf48b3e742953b6a74196cc147fe54e78dbffc96
Galshamir8 <galshamir8@gmail.com>
2023-09-28 18:20:34 +0300

    fix compliation errors

--- parallel/cuda/scoreCalculation.cu
+++ parallel/cuda/scoreCalculation.cu
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <omp.h>
-#include "../...common/common.h"
+#include "../../common/common.h"
 
 typedef struct {
   int score;
   int offset;
   int k;
-} scorePayload;
+} ScorePayload;
+
+typedef struct {
+  int maxScore;
+  int offset;
+  int k;
+  int index;
+  char sequence[MAX_CHARACTERS_SEQ];
+} ResultPayload;
 
 // Function declaration
 void calculateMaxAlignmentScore(char* mainSequence, char* sequence, ResultPayload *sendBuffer, int** scoreMat);
@@ -15,7 +23,7 @@ void calculateMaxAlignmentScore(char* mainSequence, char* sequence, ResultPayloa
 __global__ void calculate(char* mainSequence, int mainSequenceLength, char* sequence, int sequenceLength,
                           int* scoreMat, int scoreMatLength, ScorePayload* scorePayload
 ) {
-  __shared__ int results[mainSequenceLength];
+  __shared__ int results[MAX_CHARACTERS_SEQ1];
   int offset = blockIdx.x;
   int k = threadIdx.y;
 
@@ -38,7 +46,7 @@ __global__ void calculate(char* mainSequence, int mainSequenceLength, char* sequ
   __syncthreads();
   
   int sum = 0;
-  for (int i = 0 i < mainSequenceLength; i++) {
+  for (int i = 0; i < mainSequenceLength; i++) {
     sum += results[i];
   }
 
@@ -50,19 +58,25 @@ __global__ void calculate(char* mainSequence, int mainSequenceLength, char* sequ
 
 void calculateMaxAlignmentScore(char* mainSequence, char* sequence, ResultPayload *sendBuffer, int** scoreMat) {
   int* deviceScoreMat;
-  char* deviceMainSequence, deviceSequence;
+  char *deviceMainSequence, *deviceSequence;
   int mainSequenceLength = strlen(mainSequence) + 1,
       sequenceLength= strlen(sequence) + 1,
       scoreMatLength = SCORE_TABLE_ROWS * SCORE_TABLE_COLS;
-  int numOfBlocks = offset = (mainSequenceLength - sequenceLength),
+  int numOfBlocks = (mainSequenceLength - sequenceLength), 
+      offset = (mainSequenceLength - sequenceLength),
       k = sequenceLength - 1;
   dim3 threadsPerBlock(sequenceLength, k);
   int numOfScores = offset * k;
-  scorePayload* scorePayload, deviceScorePayload;
+  ScorePayload *scorePayload, *deviceScorePayload;
+
+  scorePayload = (ScorePayload*) malloc(numOfScores * sizeof(ScorePayload));
+  if (scorePayload == NULL){
+    printf("Failed to allocate score payload");
+    exit(1);
+  } 
 
   // allocate memory for cuda arguments (pointers mostly)
-
-  cudaMalloc((void**)&deviceScorePayload, numOfScores * sizeof(scorePayload));
+  cudaMalloc((void**)&deviceScorePayload, numOfScores * sizeof(ScorePayload));
   cudaMalloc((void**)&deviceMainSequence, mainSequenceLength);
   cudaMalloc((void**)&deviceSequence, sequenceLength);
   cudaMalloc((void**)&deviceScoreMat, scoreMatLength * sizeof(int));
@@ -80,13 +94,13 @@ void calculateMaxAlignmentScore(char* mainSequence, char* sequence, ResultPayloa
   );
 
   // copy result from GPU to CPU variable
-  cudaMemcpy(&scorePayload, deviceScorePayload, numOfScores * sizeof(ScorePayload), cudaMemcpyDeviceToHost);
+  cudaMemcpy(scorePayload, deviceScorePayload, numOfScores * sizeof(ScorePayload), cudaMemcpyDeviceToHost);
 
   #pragma omp declare reduction (max_alignment_score: struct ScorePayload :\
             omp_out = (omp_out.score > omp_in.scre ? omp_out : omp_in)\
             initializer(omp_priv = omp_orig)
 
-  scorePayload maxScore;
+  ScorePayload maxScore;
   maxScore.score = scorePayload[0].score;
 
   #pragma omp parallel for reduction (max_alignment_score: vmax)
@@ -98,9 +112,9 @@ void calculateMaxAlignmentScore(char* mainSequence, char* sequence, ResultPayloa
   }
 
   // enrich sndBuffer with CPU results
-  sendBuffer.offset = maxScore.offset;
-  sendBuffer.maxScore = maxScore.maxScore;
-  sendBuffer.k = maxScore.k;
+  sendBuffer->offset = maxScore.offset;
+  sendBuffer->maxScore = maxScore.score;
+  sendBuffer->k = maxScore.k;
 
   // free memory on the GPU side
   cudaFree(deviceMainSequence);
